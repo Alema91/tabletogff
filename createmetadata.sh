d210c58bfbd17487f8ce053b6a50536665534a20
alberto <alberto.lema.blanco@gmail.com>
2023-01-30 12:06:37 +0100

    Create metadata conda

--- createmetadata.sh
+++ createmetadata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# conda activate biopiton
 python getlengthfasta.py $1 $2
 cut -f2 data/length_fasta.tsv | tail -n +2 | paste -d "\t" > data/metadata.tmp
 paste -d "\t" data/JudiSeq-A25_mapping.txt data/metadata.tmp > data/metadata.tsv
